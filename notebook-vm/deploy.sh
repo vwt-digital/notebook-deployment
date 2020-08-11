e30961a10ea5861177c985a2dc3e3e9de9a6fdfd
Erik van Egmond <e.vanegmond@vwt.digital>
2020-08-11 11:01:50 +0200

    DAT-3941 Move the creation of the virtual machine to the bash script.

--- notebook-vm/deploy.sh
+++ notebook-vm/deploy.sh
@@ -12,3 +12,32 @@ else
 	gcloud deployment-manager deployments update "$2" --config config.yaml --project="$1"
 fi
 
+export INSTANCE_NAME="$1"-vm
+export VM_IMAGE_PROJECT="deeplearning-platform-release"
+export VM_IMAGE_FAMILY="common-cpu"
+export MACHINE_TYPE="n1-standard-1"
+export LOCATION="europe-west1-b"
+export NETWORK="$1"-vn
+export STARTUP_SCRIPT=gs://"$1"-data-stg/startup-script.sh
+export SERVICE_ACCOUNT=data-science@"$1".iam.gserviceaccount.com
+
+RES=$(gcloud beta notebooks instances list \
+  --location=$LOCATION \
+  --format 'value(name)'
+  )
+if [[ "$RES" == *"$1"* ]]; then
+  echo "vm $1 already exists"
+else
+  echo "creating vm " "$INSTANCE_NAME"
+  gcloud beta notebooks instances create "$INSTANCE_NAME" \
+    --vm-image-project="$VM_IMAGE_PROJECT" \
+    --vm-image-family="$VM_IMAGE_FAMILY" \
+    --machine-type="$MACHINE_TYPE" \
+    --location="$LOCATION" \
+    --network="$NETWORK" \
+    --post-startup-script="$STARTUP_SCRIPT" \
+    --service-account="$SERVICE_ACCOUNT"
+fi
+
+
+
